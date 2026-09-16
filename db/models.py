e906ad51c35b7f2e924362bf3d4945369bb8fb7f
Polishchuk Ivan <skiv08098@gmail.com>
2026-09-16 11:30:26 +0300

    fix

--- db/models.py
+++ db/models.py
@@ -73,8 +73,12 @@ class Order(models.Model):
 
 
 class Ticket(models.Model):
-    movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE, related_name="tickets")
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets_order")
+    movie_session = models.ForeignKey(MovieSession,
+                                      on_delete=models.CASCADE,
+                                      related_name="tickets")
+    order = models.ForeignKey(Order,
+                              on_delete=models.CASCADE,
+                              related_name="tickets_order")
     row = models.IntegerField()
     seat = models.IntegerField()
 
